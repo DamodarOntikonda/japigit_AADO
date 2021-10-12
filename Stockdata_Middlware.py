f088fa8c360797c0c58f7f16f2403905e54f9914
DamodarOntikonda <dontikon@asu.edu>
2021-10-11 22:43:04 -0700

    moving python and output file to git

---
+++ Stockdata_Middlware.py
@@ -0,0 +1,40 @@
+import sys
+
+from alpha_vantage.timeseries import TimeSeries
+
+API_KEY = '7YVE9DLCGCNJKT9S'
+
+def getStockdata(symbol):
+
+    try:
+        print("Collecting info....")
+
+        ts = TimeSeries(key=API_KEY, output_format='pandas')
+
+        data, meta_data = ts.get_intraday(symbol=symbol, interval='1min', outputsize='full')
+
+        return str(data.tail(1).iloc[0]['4. close'])
+
+    except:
+        return "not found"
+
+def main():
+
+    output_file = open('japi.out', 'w')    
+    while 1:
+        userInput = input("Enter Stock Symbol or EXIT to exit: ").upper()
+
+        if userInput != "EXIT":
+
+            serverData = 'The current price of {} is {}\n'.format(userInput, getStockdata(userInput))
+
+            print(serverData)
+
+            output_file.write(serverData)
+
+        else:
+            output_file.close()
+            sys.exit("\nThank you!\n")
+        #output_file.close()
+
+main()
